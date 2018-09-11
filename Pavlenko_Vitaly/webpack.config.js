f77af31596520d0f847d7b060762d5e5224af671
pavlenkovit <vitpvln@gmail.com>
2018-09-11 13:04:40 +0300

    change module rules

--- Pavlenko_Vitaly/webpack.config.js
+++ Pavlenko_Vitaly/webpack.config.js
@@ -14,16 +14,10 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
-                }
-            },
-            {
-                test: /\.css$/,
-                use: {
-                    loader: 'css-loader',
                 }
             },
             {
