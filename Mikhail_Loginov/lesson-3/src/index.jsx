eeed275a7a5f32de1d01dda15864e320c4479986
Shadow <madion86@yandex.ru>
2018-09-14 00:47:10 +0300

    lesson 2 hometask has no subfolders now

--- Mikhail_Loginov/lesson-3/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Layout from 'components/Layout';
-
-ReactDOM.render(<Layout />, document.getElementById('root'));
