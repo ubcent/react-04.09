c645a40c46a34cf91dff6235d2cc5fedb2b868cc
Yulya Vasilyeva <043@tut.by>
2018-09-14 22:19:55 +0300

    Из bootstrap добавлена форма авторизации

--- Yulya_Vasilyeva/src/js/components/SignIn/SignIn.jsx
+++ Yulya_Vasilyeva/src/js/components/SignIn/SignIn.jsx
@@ -4,10 +4,30 @@ import './SignIn.css';
 //импорт React
 import React, { Component } from 'react';
 
-export default class SignIn extends Component{
-    render(){
-        return(
-           <p>авторизация</p>
+
+import { //импорт формы из Bootstrap
+    Button,
+    Form,
+    Label,
+    Input,
+    FormGroup
+} from 'reactstrap';
+
+export default class SignIn extends Component {
+    render() {
+        return (
+            <Form className="signin">
+                <FormGroup>
+                    <Label for="login">Логин</Label>
+                    <Input type="text" name="user_login" id="login" placeholder="vasya01" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="pass">Пароль</Label>
+                    <Input type="password" name="user_pass" id="pass" placeholder="123" />
+                </FormGroup>
+                <Button color="primary" block className="btn-signin">Войти</Button>
+
+            </Form>
         );
     }
 }
