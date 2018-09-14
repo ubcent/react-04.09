cf9bb5bc8d216b2cbec4c7ff6d5d27eaad0b5b26
Yulya Vasilyeva <043@tut.by>
2018-09-14 21:49:10 +0300

    Добавлена структура страницы

--- Yulya_Vasilyeva/src/js/_index.jsx
+++ Yulya_Vasilyeva/src/js/_index.jsx
@@ -8,20 +8,34 @@ import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
 
 //компоненты Bootstrap
-import {Container} from 'reactstrap';
+import {Container, Row, Col} from 'reactstrap';
 
 //компоненты страницы
 import Menu from 'components/Menu/Menu';
+import Descr from 'components/Descr/Descr';
+import Article from 'components/Article/Article';
+import SignIn from 'components/SignIn/SignIn';
+import Footer from 'components/Footer/Footer';
 
 //Главный компонет
 class App extends Component{
     render(){
         //const {color} = this.props;
-        //Собираем компонтенты: меню + авторизация + список статей
+        //Собираем компонтенты: меню + авторизация + список статей + футер
         return(
             <Fragment>
                <Container>
                    <Menu/>
+                    <Row>
+                        <Col md="8">
+                            <Descr/>
+                            <Article/>
+                        </Col>
+                        <Col md="auto">
+                            <SignIn/>
+                        </Col>
+                    </Row>
+                    <Footer/>
                </Container>
             </Fragment>
         );
