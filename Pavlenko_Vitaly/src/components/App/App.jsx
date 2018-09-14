bcd010fafcc39cde2624dee63a3ebe33da816895
pavlenkovit <vitpvln@gmail.com>
2018-09-14 21:48:11 +0300

    change structure application

--- Pavlenko_Vitaly/src/components/App/App.jsx
+++ Pavlenko_Vitaly/src/components/App/App.jsx
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
-import Layout from 'layouts/Layout';
-import MainPage from 'pages/MainPage';
+import Layout from 'Pavlenko_Vitaly/src/layouts/Layout/index';
+import MainPage from 'Pavlenko_Vitaly/src/pages/MainPage/index';
 
 export default class App extends Component {
 
