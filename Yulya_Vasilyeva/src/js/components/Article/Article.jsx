71652887f4df0dcb3b8a18418f55eacad84c1fdf
Yulya Vasilyeva <043@tut.by>
2018-09-14 21:49:27 +0300

    Компонент списка статей

---
+++ Yulya_Vasilyeva/src/js/components/Article/Article.jsx
@@ -0,0 +1,13 @@
+//импорт пользовательских стилей для редактирования статей
+import './Article.css';
+
+//импорт React
+import React, { Component } from 'react';
+
+export default class Article extends Component{
+    render(){
+        return(
+            <p>статьи</p>
+        );
+    }
+}
