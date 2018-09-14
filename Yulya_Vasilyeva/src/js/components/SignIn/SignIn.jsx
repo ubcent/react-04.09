9846b491f13192e501caf9d366bca0817a70cb7b
Yulya Vasilyeva <043@tut.by>
2018-09-14 21:49:58 +0300

    Компонент для авторизации

---
+++ Yulya_Vasilyeva/src/js/components/SignIn/SignIn.jsx
@@ -0,0 +1,13 @@
+//импорт пользовательских стилей для редактирования авторизации
+import './SignIn.css';
+
+//импорт React
+import React, { Component } from 'react';
+
+export default class SignIn extends Component{
+    render(){
+        return(
+           <p>авторизация</p>
+        );
+    }
+}
