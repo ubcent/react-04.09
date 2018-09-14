9929d8f894c246e51f3b3cbf5b82a9c1e1709438
Yulya Vasilyeva <043@tut.by>
2018-09-14 18:42:07 +0300

    Подключение Bootstrap

--- Yulya_Vasilyeva/src/js/_index.jsx
+++ Yulya_Vasilyeva/src/js/_index.jsx
@@ -1,17 +1,23 @@
-//стили
+//Имортируем стили для BOOTSTRAP
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //компоненты React
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-//свои компоненты
+//компоненты Bootstrap
+import {Button} from 'reactstrap';
 
 
 //Главный компонет
 class App extends Component{
     render(){
+        const {color} = this.props;
         return(
-            <div>Проверка</div>
+            <div>
+                Проверка
+                <Button color="danger">Danger</Button>
+            </div>
         );
     }
 }
