c999d4b348d5ee19a9d8856b4d4bcd88eb34a92b
Ap-of-an <dmitry.virtyal@gmail.com>
2018-09-15 18:10:04 +0700

    les3

---
+++ BelikDmitry/src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import './Login.css'
+
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  render() {
+     return (
+      <p className="btn-login">Войти</p>
+     );
+  }
+}
