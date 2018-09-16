470ec7ccfb8b132f68d1cee67e5edc7558a7afd4
Behuamuh <sven1980@list.ru>
2018-09-16 18:09:15 +0300

    Добавлена заготовка чата для доп ДЗ к уроку 4

---
+++ Shitikov_Veniamin_chat/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const ExptacrTextPlugin = require('extract-text-webpack-plugin');
+const HTMLPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'src', 'index.jsx')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            components: path.resolve(__dirname, 'src', 'components')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ExptacrTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                }),
+            }
+        ]
+    },
+    plugins: [
+        new ExptacrTextPlugin({filename: 'style.css'}),
+        new HTMLPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html'
+        })
+    ]
+}
