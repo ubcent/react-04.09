dce45238aece80e1382a188a1c12b2d7c761181d
Yulya Vasilyeva <043@tut.by>
2018-09-17 19:48:16 +0300

    конфигурация проекта

---
+++ Yulya_Vasilyeva_messenger/webpack.config.js
@@ -0,0 +1,88 @@
+//путь проекта
+const path = require('path');
+
+//объединяет css файлы в один
+const MiniCssExtract = require('mini-css-extract-plugin');
+//автоматически ставит префикты в css
+const AutoPrefixer = require('autoprefixer');
+//добавляем в HTML файл необходимые пути для css и js
+const HtmlPlugin = require('html-webpack-plugin');
+//чистим папку dist
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        entry: path.resolve(__dirname, 'src', 'js/_index.jsx'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/index.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            components: path.resolve(__dirname, 'src', 'js/components'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                        { loader: 'style-loader'},
+                        { loader: MiniCssExtract.loader},
+                        { loader: 'css-loader'},
+                        { loader: 'postcss-loader',
+                            options: {
+                                plugins: [
+                                    AutoPrefixer({
+                                        browsers:['ie >= 8', 'last 4 version']
+                                    })
+                                ],
+                            }
+                        },
+                        { loader: 'clean-css-loader'},
+                        { loader: 'sass-loader'},
+                    ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'fonts/[name].[ext]',
+                    }
+                }
+
+            },
+            {
+                test: /\.(svg|gif|jpe?g|png)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options:{
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                        },
+                    },
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtract({filename: 'css/min.style.css'}),
+        new HtmlPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html'
+        }),
+        new CleanPlugin(['dist']),
+    ]
+}
