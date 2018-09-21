99def7c8d4e1f943fb4a0787505c8005097afbf8
Yulya Vasilyeva <043@tut.by>
2018-09-22 00:29:13 +0300

    Добавлена возможность добавлять сообщения в чат через JSON

--- Yulya_Vasilyeva_messenger/src/components/MessageWindow/SendForm/SendForm.jsx
+++ Yulya_Vasilyeva_messenger/src/components/MessageWindow/SendForm/SendForm.jsx
@@ -10,30 +10,37 @@ export default class SendForm extends PureComponent {
 
         this.state = {
             userMessage: '',
-            valueText: '',
         }
     }
 
     //изменение формы с сообщением
     changeInput = (ev) => {
-        const { userMessage } = this.state;
         this.setState({
             [ev.target.name]: ev.target.value,
         });
     }
 
     sendMessage = () => {
-        const { userMessage, valueText } = this.state;
-        this.setState({
-            valueText: '',
-        });
+        const { userMessage } = this.state;
+        const url = 'http://127.0.0.1:3000/chat';
+        fetch(url, {
+            method: 'post',
+            headers: {  
+                "Content-type": "application/json"  
+              },
+              body: JSON.stringify({
+                user: 'right',
+                message: userMessage,
+                data: '01-02-2018'
+              })
+        })
+        .then(resp => resp.json())
     }
 
     render() {
-        const { dataMsg } = this.props;
         return (
             <form action="#" className="send-form">
-                <textarea id="message" name="userMessage" className="send-message" value={this.valueText}></textarea>
+                <textarea onChange={this.changeInput} id="message" name="userMessage" className="send-message"></textarea>
                 <input onClick={this.sendMessage} type="submit" className="send-button" value="Отправить"/>
             </form>
         );
