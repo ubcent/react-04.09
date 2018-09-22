0d82cdb9a50cd78bc1a20571da66361b0f3bb322
serserch <serserch@yandex.ru>
2018-09-22 11:18:36 +0300

    Add file-loader

--- Eremenko/webpack.config.js
+++ Eremenko/webpack.config.js
@@ -3,50 +3,72 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  //точка входа
-  entry: {
-    main: path.resolve(__dirname, 'src', 'index.jsx'),
-  },
-  //папка куда собирается
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  //пути к папкам компонентов
-  resolve: {
-    //что бы не указывать расширение файла
-    extensions: ['.js', '.jsx'],
-    alias: {
-      components: path.resolve(__dirname, 'src', 'components'),
+    //точка входа
+    entry: {
+        main: path.resolve(__dirname, 'src', 'index.jsx'),
     },
-  },
-  //сама сборка
-  module: {
-    //правила для сборки
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+    //папка куда собирается
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    //пути к папкам компонентов
+    resolve: {
+        //что бы не указывать расширение файла
+        extensions: ['.js', '.jsx'],
+        alias: {
+            components: path.resolve(__dirname, 'src', 'components'),
         },
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader'],
+    },
+    //сама сборка
+    module: {
+        //правила для сборки
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader'],
+                }),
+            },
+            {
+                test: /\.(gif|png|jpe?g)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'img/[hash].[ext]',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'fonts/[hash].[ext]'
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'style.css',
         }),
-      },
+        new HtmlPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        }),
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: 'style.css',
-    }),
-    new HtmlPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-      filename: 'index.html',
-    }),
-  ]
 }
