b22835a3e695a7d5e2b136758461c2f0e83e1076
AntoxaVolkov <m-a-r-v-i-n@mail.ru>
2018-09-22 15:02:53 +0300

    Lesson 5 - Start No finish

--- AntonVolkov/webpack.config.js
+++ AntonVolkov/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
         alias: {
             components: path.resolve(__dirname, 'src', 'components'),
             pages: path.resolve(__dirname, 'src', 'pages'),
+            models: path.resolve(__dirname, 'src', 'models'),
         }
     },
     module: {
