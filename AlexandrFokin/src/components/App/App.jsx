d282008c7906be63effe902e58b977cd47516811
KiNGPiN <sfy@mail.ru>
2018-09-28 00:03:58 +0300

    подключение redux к приложению

--- AlexandrFokin/src/components/App/App.jsx
+++ AlexandrFokin/src/components/App/App.jsx
@@ -2,10 +2,12 @@ import './App.scss';
 
 import React, { PureComponent } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 import Header from 'components/Header';
 
 import routes from '../../routes';
+import store from 'store';
 
 /**
  * Класс App - корневой компонент, отображающий страницы сайта
@@ -13,15 +15,17 @@ import routes from '../../routes';
 export default class App extends PureComponent {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          {/* Header */}
-          <Header/>
-          <Switch>
-            {routes.map((route, idx) => <Route key={idx} {...route}/>)}
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            {/* Header */}
+            <Header/>
+            <Switch>
+              {routes.map((route, idx) => <Route key={idx} {...route}/>)}
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
