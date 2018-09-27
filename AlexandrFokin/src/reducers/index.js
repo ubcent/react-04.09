1a37a664d0a37260f313e16afc00de21076fa72c
KiNGPiN <sfy@mail.ru>
2018-09-28 00:01:16 +0300

    корневой редюсер

---
+++ AlexandrFokin/src/reducers/index.js
@@ -0,0 +1,11 @@
+import {combineReducers} from 'redux';
+import messagesReducer from './messages';
+
+/**
+ * Корневой reducer, управляющий всеми reducers
+ */
+export default combineReducers({
+  // устанавливаем reducer, управляющий хранением сообщений messages
+  // messages - наименование обрабатываемого action
+  messages: messagesReducer,
+})
