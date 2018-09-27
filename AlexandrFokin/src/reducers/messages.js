ae4a373499ffc89dd17371560c54dc002aac5501
KiNGPiN <sfy@mail.ru>
2018-09-28 00:01:37 +0300

    редюсер для обработки сообщений

---
+++ AlexandrFokin/src/reducers/messages.js
@@ -0,0 +1,28 @@
+import {handleActions} from 'redux-actions';
+// Action, отправляющий полученное сообщение в store
+import {messageReceived} from 'actions/messages';
+// Значение состояния по умолчанию
+const initialState = {
+  // массив для хранения объектов сообщений
+  entities: [],
+};
+
+/**
+ * Reducer для хранения сообщений в store
+ */
+export default handleActions({
+  // устанавливает соответствие между action и обработчиком
+  // messageReceived - наименование обрабатываемого action
+  // (state, action) - функция-обработчик
+  // state - предыдущее состояние до прихода сообщения
+  // action - объект действия, содержащий параметры
+  [messageReceived]: (state, action) => {
+    return {
+      // старое значение состояния
+      ...state,
+      // добавляем новое сообщение в массив сообщений
+      // payload - хранит параметр, указанный при вызове action, т.е. message (сообщение)
+      entities: state.entities.concat([action.payload]),
+    }
+  }
+}, initialState);
