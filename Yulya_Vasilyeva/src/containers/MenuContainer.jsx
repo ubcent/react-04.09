bd8daf4779531c5e8c27a7dcaee7e9c0f914f8a5
Yulya Vasilyeva <043@tut.by>
2018-09-28 19:53:40 +0300

    удалено изменение свойства, в нем нет смысла

--- Yulya_Vasilyeva/src/containers/MenuContainer.jsx
+++ Yulya_Vasilyeva/src/containers/MenuContainer.jsx
@@ -41,8 +41,7 @@ export default class MenuContainer extends PureComponent {
         // получаем меню с сервера
         requestData('menu').then((menu) => {
             this.setState({
-                menu, 
-                update: false
+                menu
             });
         })
     }
