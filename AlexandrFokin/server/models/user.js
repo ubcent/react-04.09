9da6142569cdb23c097b7cd50ab00e245c298905
KiNGPiN <sfy@mail.ru>
2018-10-06 18:15:40 +0300

    Добавлена схема для объекта User (пользователь)

--- AlexandrFokin/server/models/user.js
+++ AlexandrFokin/server/models/user.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
+// подключаем интерфейс Schema для создания модели
 const Schema = mongoose.Schema;
 
+// схема объекта User
 const userSchema = new Schema({
-  name: {
+  // свойство userName
+  userName: {
+    // тип данных
     type: String,
-    default: 'Ivan',
+    // флаг обязательного поля
     required: true,
-  }
+  },
+  // свойство userMail
+  userMail: {
+    type: String,
+    required: true,
+  },
+  userBirthday: {
+    type: Date,
+  },
 });
 
+// экспорт модели User
 module.exports = mongoose.model('User', userSchema);
